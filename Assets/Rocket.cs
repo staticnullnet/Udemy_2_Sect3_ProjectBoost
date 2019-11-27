903f9484ff4b43b4d1420f64d6637087326e48a3
Sky <skylerlusc@gmail.com>
2019-11-27 18:07:24 -0500

    Added collision detection and fuel amount

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -7,31 +7,65 @@ public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
     AudioSource engineAudio;
-    public float fwdThrust = 1;
-    public float rcsThrust = 100f;
+    [SerializeField] float fwdThrust = 1f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float fuelLossThrust = 0.1f;
+    [SerializeField] float maxFuel = 100f;
+    float currentFuel;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         engineAudio = GetComponent<AudioSource>();
+        //fuelCounter = GetComponent<Text>();
+
+        currentFuel = maxFuel;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Thrust();
-        Rotate();
+        Rotate();        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Fuel":
+                if (currentFuel + 20 > maxFuel)
+                    currentFuel = maxFuel;
+                else
+                    currentFuel += 20f;
+                GameObject.Destroy(collision.gameObject);
+                break;
+            case "Finish":
+                print("WIN");
+
+                break;
+            default:
+                print("dead");
+                break;
+        }
     }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space)) //Can thrust while rotating
         {
-            rigidBody.AddRelativeForce(Vector3.up * fwdThrust);
-            if (!engineAudio.isPlaying)
+            if (currentFuel > 0)
             {
-                engineAudio.Play();
+                rigidBody.AddRelativeForce(Vector3.up * fwdThrust);
+                currentFuel -= fuelLossThrust;
+                print(currentFuel);
+
+                if (!engineAudio.isPlaying)
+                {
+                    engineAudio.Play();
+                }
             }
         }
         else
@@ -47,7 +81,7 @@ public class Rocket : MonoBehaviour
         //Can only turn one direction
         if (Input.GetKey(KeyCode.A))
         {            
-            transform.Rotate(Vector3.forward * rotationThisFrame);
+            transform.Rotate(Vector3.forward * rotationThisFrame);            
         }
         else if (Input.GetKey(KeyCode.D))
         {        
