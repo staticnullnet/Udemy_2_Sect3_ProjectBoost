8b0f6ebbc4429705ee68bb527db853cdb4d918de
Sky <skylerlusc@gmail.com>
2019-11-27 07:43:35 -0500

    Add engine audio on  thrust

---
+++ Assets/Rocket.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    AudioSource engineAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        engineAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //Can thrust while rotating
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            if (!engineAudio.isPlaying)
+            {
+                engineAudio.Play();
+            }
+        }
+        else
+        {
+            engineAudio.Stop();
+        }
+        
+        //Can only turn one direction
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("ROTATE LEFT");
+            //rigidBody.MoveRotation(Quaternion.AngleAxis(10, Vector3.zero));
+            
+
+            
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("ROTATE RIGHT");
+        }
+    }
+}
