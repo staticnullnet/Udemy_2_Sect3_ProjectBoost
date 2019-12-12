9301292d17b0970b52fa0ff26857207511443907
Sky <skylerlusc@gmail.com>
2019-12-11 22:43:59 -0500

    Added particle and sound effects for engine, winning, and dying

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,11 +5,22 @@ using UnityEngine.SceneManagement;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
-    AudioSource engineAudio;
+    [SerializeField] AudioSource audioSource;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip succeed;
+
+    [SerializeField] ParticleSystem mainEngine1Particles;
+    [SerializeField] ParticleSystem mainEngine2Particles;
+    [SerializeField] ParticleSystem deathParticles;
+    [SerializeField] ParticleSystem succeedParticles;
+
     [SerializeField] float fwdThrust = 1f; //Forward Thrust coefficient
     [SerializeField] float rcsThrust = 100f; //Rotation Thrust coefficient
     [SerializeField] float fuelLossThrust = 0.05f; //fuel loss coefficient    
     [SerializeField] float maxFuel = 100f; //maximum fuel coefficient
+    
     State state = State.Alive;
     enum State { Alive, Transcending, Dieing };
     public float CurrentFuel { get; set; }
@@ -19,9 +30,11 @@ public class Rocket : MonoBehaviour
     void Start()
     {        
         rigidBody = GetComponent<Rigidbody>();
-        engineAudio = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
         CurrentFuel = MaxFuel;
-        
+        mainEngine1Particles.Stop();
+        mainEngine2Particles.Stop();
+
     }
 
     // Update is called once per frame
@@ -29,8 +42,8 @@ public class Rocket : MonoBehaviour
     {
         if (state == State.Alive)
         {
-            Thrust();
-            Rotate();
+            RespondToThrustInput();
+            RespondToRotateInput();
         }
     }
 
@@ -39,18 +52,34 @@ public class Rocket : MonoBehaviour
         if (state != State.Alive) { return; }
 
         switch (collision.gameObject.tag)
-        {            
+        {
             case "Finish":
-                state = State.Transcending;
-                Invoke("LoadNextLevel", 1f); //Better to use coroutines. StartCoroutine("LoadNextScene(1)"); ??WIP 
+                StartSuccessSequence();
                 break;
             case "Friendly":
                 break;
             default:
-                state = State.Transcending;
-                Invoke("LoadFirstLevel", 1f); //Better to use coroutines. StartCoroutine("LoadNextScene(1)"); ??WIP 
+                StartDeathSequence();
                 break;
         }
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dieing;
+        deathParticles.Play();
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadFirstLevel", 1f); //Better to use coroutines. StartCoroutine("LoadNextScene(1)"); ??WIP 
+    }
+
+    private void StartSuccessSequence()
+    {
+        state = State.Transcending;
+        succeedParticles.Play();
+        audioSource.Stop();
+        audioSource.PlayOneShot(succeed);
+        Invoke("LoadNextLevel", 1f); //Better to use coroutines. StartCoroutine("LoadNextScene(1)"); ??WIP 
     }
 
     private void LoadNextLevel()
@@ -63,29 +92,43 @@ public class Rocket : MonoBehaviour
         SceneManager.LoadScene(0);
     }
 
-    private void Thrust()
+    private void RespondToThrustInput()
     {
         if (Input.GetKey(KeyCode.Space) && CurrentFuel > 0) //Can thrust while rotating
         {
-            
-            rigidBody.AddRelativeForce(Vector3.up * fwdThrust);
-            CurrentFuel -= fuelLossThrust;
-
-            if (!engineAudio.isPlaying)
-            {
-                engineAudio.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            engineAudio..Stop();
+            audioSource.Stop();
+            mainEngine1Particles.Stop();
+            mainEngine2Particles.Stop();
         }
     }
-    private void Rotate()
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * fwdThrust);
+        CurrentFuel -= fuelLossThrust;
+
+        if (!audioSource.isPlaying)
+        {
+            //Used to play audio but can create skipping due to start/stopping 
+            //Also plays audio source's directly attached clip.
+            //engineAudio.Play(); old code. 
+
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngine1Particles.Play();
+        mainEngine2Particles.Play();
+    }
+
+
+    private void RespondToRotateInput()
     {
         if (CurrentFuel <= 0)
         {
-            engineAudio.Stop();
+            audioSource.Stop();
             return;
         }
 
